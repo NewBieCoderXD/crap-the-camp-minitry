a70c94e522af5308ce239c3100a48f51c58d17bb
NewBieCOderXD <fruity5555@gmail.com>
2024-02-02 21:54:01 +0700

    add Drop table

--- commands.sql
+++ commands.sql
@@ -1,3 +1,13 @@
+DROP TABLE IF EXISTS "delete";
+DROP TABLE IF EXISTS "owns";
+DROP TABLE IF EXISTS "Transaction" CASCADE;
+DROP TABLE IF EXISTS "User" CASCADE;
+DROP TABLE IF EXISTS "Customer" CASCADE;
+DROP TABLE IF EXISTS "Admin" CASCADE;
+DROP TABLE IF EXISTS "PlaceOwner" CASCADE;
+DROP TABLE IF EXISTS "Place" CASCADE;
+DROP TABLE IF EXISTS "Booking" CASCADE;
+
 CREATE TABLE "User"(
   email VARCHAR(320) PRIMARY KEY,
   name VARCHAR NOT NULL,
@@ -7,14 +17,14 @@ CREATE TABLE "User"(
 
 CREATE TABLE "Customer"(
   email VARCHAR(320) UNIQUE NOT NULL REFERENCES "User" (email),
-  customerId INTEGER UNIQUE NOT NULL,
-  PRIMARY KEY(email,customerId)
+  customer_id INTEGER UNIQUE NOT NULL,
+  PRIMARY KEY(email,customer_id)
 );
 
 CREATE TABLE "Admin"(
   email VARCHAR(320) UNIQUE NOT NULL REFERENCES "User" (email),
-  employeeId INTEGER UNIQUE NOT NULL,
-  PRIMARY KEY(email,employeeId)
+  employee_id INTEGER UNIQUE NOT NULL,
+  PRIMARY KEY(email,employee_id)
 );
 
 CREATE TABLE "PlaceOwner"(
@@ -31,19 +41,19 @@ CREATE TABLE "Place"(
 
 CREATE TABLE "Booking"(
    in_date DATE NOT NULL,
-   customerId INTEGER NOT NULL REFERENCES "Customer" (customerId),
+   customer_id INTEGER NOT NULL REFERENCES "Customer" (customer_id),
    address VARCHAR NOT NULL REFERENCES "Place" (address),
    price DECIMAL NOT NULL,
    out_date DATE NOT NULL,
-   PRIMARY KEY (in_date, customerId, address)
+   PRIMARY KEY (in_date, customer_id, address)
 );
 
 CREATE TABLE "Transaction"(                                                                    
    in_date DATE NOT NULL,                                                                                              
-   customerId INTEGER NOT NULL REFERENCES "Customer" (customerId),                                                     
+   customer_id INTEGER NOT NULL REFERENCES "Customer" (customer_id),                                                     
    address VARCHAR NOT NULL REFERENCES "Place" (address),                                                              
    pay_date DATE NOT NULL,                                                                                             
-   PRIMARY KEY (in_date, customerId, address)                                                                          
+   PRIMARY KEY (in_date, customer_id, address)                                                                          
 );    
 
 CREATE TABLE "owns"(                                                                           
@@ -54,10 +64,19 @@ CREATE TABLE "owns"(
 
 CREATE TABLE "delete"(
    address VARCHAR NOT NULL REFERENCES "Place" (address),
-   employeeId INTEGER NOT NULL REFERENCES "Admin" (employeeId),
-   customerId INTEGER NOT NULL REFERENCES "Customer" (customerId),
+   employee_id INTEGER NOT NULL REFERENCES "Admin" (employee_id),
+   customer_id INTEGER NOT NULL REFERENCES "Customer" (customer_id),
    in_date DATE NOT NULL,
-   PRIMARY KEY (address,employeeId,customerId,in_date),
-   FOREIGN KEY (customerId,address,in_date) REFERENCES "Booking" (customerId,address,in_date)
+   PRIMARY KEY (address,employee_id,customer_id,in_date),
+   FOREIGN KEY (customer_id,address,in_date) REFERENCES "Booking" (customer_id,address,in_date)
 );
 
+CREATE OR REPLACE FUNCTION on_booking_delete()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+$$
+BEGIN
+   INSERT INTO "delete" VALUES (OLD.address,OLD.employee_id,OLD.customer_id,OLD.in_date);
+   RETURN NEW;
+END;
+$$;
