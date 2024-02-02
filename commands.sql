ce53c6ab2a24c5fcc70b9660757024681256c8ae
NewBieCoderXD <fruity5555@gmail.com>
2024-02-02 22:12:56 +0700

    make on_booking_delete not error(not tested) 2

--- commands.sql
+++ commands.sql
@@ -74,6 +74,7 @@ CREATE TABLE "delete"(
 CREATE OR REPLACE FUNCTION on_booking_delete()
 RETURNS TRIGGER
 LANGUAGE plpgsql
+AS
 $$
 BEGIN
    INSERT INTO "delete" VALUES (OLD.address,OLD.employee_id,OLD.customer_id,OLD.in_date);
