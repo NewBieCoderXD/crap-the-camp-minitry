a01b954a3d66e0666b390f5b6e33f72c5b7075a8
NewBieCoderXD <fruity5555@gmail.com>
2024-02-03 13:27:17 +0700

    edit procedure

--- commands.sql
+++ commands.sql
@@ -7,6 +7,7 @@ DROP TABLE IF EXISTS "Admin" CASCADE;
 DROP TABLE IF EXISTS "PlaceOwner" CASCADE;
 DROP TABLE IF EXISTS "Place" CASCADE;
 DROP TABLE IF EXISTS "Booking" CASCADE;
+DROP TABLE IF EXISTS "edit" CASCADE;
 DROP PROCEDURE delete_booking;
 
 CREATE TABLE "User"(
@@ -63,11 +64,72 @@ CREATE TABLE "owns"(
 CREATE TABLE "delete"(
    address VARCHAR NOT NULL REFERENCES "Place" (address),
    deleter_email VARCHAR NOT NULL REFERENCES "User" (email),
-   -- employee_id INTEGER NOT NULL REFERENCES "Admin" (employee_id),
    booking_owner_id INTEGER NOT NULL REFERENCES "Customer" (customer_id),
    in_date DATE NOT NULL,
-   PRIMARY KEY (address,deleter_email,booking_owner_id,in_date)
+   out_date DATE NOT NULL,
+   price DECIMAL NOT NULL,
+   delete_timestamp TIMESTAMP NOT NULL,
+   PRIMARY KEY (address,deleter_email,booking_owner_id,delete_timestamp)
 );
+
+CREATE TABLE "edit"(
+   address VARCHAR NOT NULL REFERENCES "Place" (address),
+   editor_email VARCHAR NOT NULL REFERENCES "User" (email),
+   booking_owner_id INTEGER NOT NULL REFERENCES "Customer" (customer_id),
+   in_date DATE NOT NULL,
+   out_date DATE NOT NULL,
+   price DECIMAL NOT NULL,
+   edit_timestamp TIMESTAMP NOT NULL,
+   PRIMARY KEY (address,editor_email,booking_owner_id,edit_timestamp),
+   FOREIGN KEY (address,booking_owner_id,in_date) REFERENCES "Booking"(address,customer_id,in_date)
+);
+
+CREATE OR REPLACE PROCEDURE edit_booking(
+   editor_email VARCHAR,
+   booking_owner_id INTEGER,
+   old_in_date DATE,
+   old_address VARCHAR,
+   
+   new_in_date DATE,
+   new_out_date DATE,
+   new_address VARCHAR,
+   new_price DECIMAL
+)
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+   old_price DECIMAL;
+   old_out_date DATE;
+BEGIN
+   IF NOT EXISTS (
+      SELECT 1 FROM "Booking"
+      WHERE old_in_date="Booking".in_date
+      AND booking_owner_id="Booking".customer_id
+      AND old_address="Booking".address
+   ) THEN
+      RAISE EXCEPTION 'Booking doesn''t exist';
+   -- check if deleter is booking's owner or admin
+   ELSIF editor_email <> (
+      SELECT email FROM "Customer"
+      WHERE booking_owner_id="Customer".customer_id
+   ) AND NOT EXISTS(
+      SELECT 1 FROM "Admin"
+      WHERE editor_email="Admin".email
+   ) THEN
+      RAISE EXCEPTION 'No permission';
+   ELSE
+      SELECT "Booking".price, "Booking".out_date INTO old_price,old_out_date FROM "Booking" 
+      WHERE "Booking".in_date=old_in_date
+      AND "Booking".customer_id=booking_owner_id
+      AND "Booking".address=old_address;
+
+      INSERT INTO edit VALUES (old_address,editor_email,booking_owner_id,old_in_date,old_out_date,old_price,NOW());
+      UPDATE "Booking"
+      SET "Booking".
+   END IF;
+END;
+$$;
 
 CREATE OR REPLACE PROCEDURE delete_booking(
    deleter_email VARCHAR,
@@ -78,6 +140,9 @@ CREATE OR REPLACE PROCEDURE delete_booking(
 LANGUAGE plpgsql
 AS
 $$
+DECLARE
+   price DECIMAL;
+   out_date DATE;
 BEGIN
    IF NOT EXISTS (
       SELECT 1 FROM "Booking"
@@ -96,13 +161,20 @@ BEGIN
    ) THEN
       RAISE EXCEPTION 'No permission';
    ELSE
+      SELECT "Booking".price, "Booking".out_date INTO price,out_date FROM "Booking" 
+      WHERE "Booking".in_date=deleted_in_date
+      AND "Booking".customer_id=booking_owner_id
+      AND "Booking".address=deleted_address;
+
+      RAISE NOTICE '% %', price, out_date;
+
       DELETE FROM "Booking"
       WHERE "Booking".in_date=deleted_in_date
       AND "Booking".customer_id=booking_owner_id
       AND "Booking".address=deleted_address;
 
       INSERT INTO "delete"
-      VALUES (deleted_address,deleter_email,booking_owner_id,deleted_in_date);
+      VALUES (deleted_address,deleter_email,booking_owner_id,deleted_in_date,out_date,price,NOW());
    END IF;
 END;
 $$;
--- dummy_inserts.sql
+++ dummy_inserts.sql
@@ -7,24 +7,25 @@ INSERT INTO "User" VALUES
 ('456@gmail.com','ahfong naja','#$NWKFD','000-0000-000');
 
 INSERT INTO "Customer" VALUES 
-('gg@gmail.com','1'),
-('ga@gmail.com','2');
+('gg@gmail.com',1),
+('ga@gmail.com',2);
 
 INSERT INTO "Admin" VALUES 
-('123@gmail.com','1'),
-('456@gmail.com','2');
+('123@gmail.com',1),
+('456@gmail.com',2);
 
 INSERT INTO "Place" VALUES 
-('งหวา',50,'000-0000-000');
+('บางหว้า',50,'000-0000-000');
 
 INSERT INTO "Booking" VALUES 
-('2023-05-11','1','งหวา',50,'2023-05-13');
+('2023-05-11','1','บางหว้า',50,'2023-05-13');
 
--- DELETE FROM "Booking" WHERE customer_id=1;
-CALL delete_booking('123@gmail.com','2023-05-11','1','งหวา');
+CALL edit_booking('456@gmail.com',1,'2023-05-11','บางหว้า', '2023-05-12','2023-05-13','บางหว้า',50);
+-- CALL delete_booking('123@gmail.com','2023-05-11','1','บางหว้า');
 
 SELECT * FROM "Booking";
 
-SELECT * FROM "delete";
+SELECT * FROM "edit";
+-- SELECT * FROM "delete";
 
 COMMIT;
