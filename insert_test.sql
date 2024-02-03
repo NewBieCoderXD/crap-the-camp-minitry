84dff0c7ee7d5679189a74111ad9c6ced99a5d6a
NewBieCoderXD <fruity5555@gmail.com>
2024-02-03 21:08:17 +0700

    add insert test

--- insert_test.sql
+++ insert_test.sql
@@ -1,5 +1,11 @@
 INSERT INTO "User" VALUES
 ('1@gmail.com','ahfong naja','#$NWKFD','000-0000-000');
 
+INSERT INTO "Customer" VALUES 
+('1@gmail.com',1);
+
+INSERT INTO "Place" VALUES 
+('บางหว้า',50,'000-0000-000',20);
+
 INSERT INTO "Booking" VALUES
 ('2023-05-11','1','บางหว้า','2023-05-15');
